f7d3a5c903a699ecfc3872cd9a6935b9f0da777c
Jeoffrey Prado <jeoffreyprado@hotmail.com>
2026-02-12 11:30:47 -0600

    Page updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import thinkingPenguin from "./thinking_penguin.png";
 
 function App() {
   const [response, setResponse] = useState("");
@@ -10,16 +11,21 @@ function App() {
 
   return (
     <div className="container">
+      {/* Penguin emojis in corners */}
+      <span className="penguin top-left">🐧</span>
+      <span className="penguin top-right">🐧</span>
+      <span className="penguin bottom-left">🐧</span>
+      <span className="penguin bottom-right">🐧</span>
+
       <div className="card">
-        <h2>PLEASE...</h2>
-        <img src="/dog.jpg" alt="Cute Dog" className="dog-img" />
-        <p className="message">Nicoool, will you be my Valentine?</p>
+        <p className="message">Nicooool, will you be my Valentine?</p>
+        <img src={thinkingPenguin} alt="Thinking Penguin" className="penguin-img"/>
         <div className="buttons">
-          <button className="yes" onClick={() => handleResponse("Absolutely 💖")}>
-            Absolutely 💖
+          <button className="yes" onClick={() => handleResponse("Absolutely 💕")}>
+            Absolutely 💕
           </button>
-          <button className="maybe" onClick={() => handleResponse(" Nooo... 😢")}>
-            Nooo... 😢
+          <button className="maybe" onClick={() => handleResponse("Noo... 😢")}>
+            Noo... 😢
           </button>
         </div>
         <p className="response">{response}</p>
